d8a2d069a84a441d78944f96e1ce3524930a72e0
Seth Chisamore <schisamo@opscode.com>
2012-12-18 14:57:06 -0500

    we don't always want to exit on reconfigure calls

--- lib/omnibus-ctl.rb
+++ lib/omnibus-ctl.rb
@@ -260,14 +260,18 @@ module Omnibus
 
     def show_config(*args)
       status = run_command("#{base_path}/bin/chef-solo -c #{base_path}/embedded/cookbooks/solo.rb -j #{base_path}/embedded/cookbooks/show-config.json -l fatal")
-      exit! 0
+      if status.success?
+        exit! 0
+      else
+        exit! 1
+      end
     end
 
-    def reconfigure(*args)
+    def reconfigure(exit_on_success=true)
       status = run_command("#{base_path}/bin/chef-solo -c #{base_path}/embedded/cookbooks/solo.rb -j #{base_path}/embedded/cookbooks/dna.json")
       if status.success?
         log "#{display_name} Reconfigured!"
-        exit! 0
+        exit! 0 if exit_on_success
       else
         exit! 1
       end
