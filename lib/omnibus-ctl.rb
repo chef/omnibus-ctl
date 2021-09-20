82599a7137627e6dada95753aad4568bdfcc3f92
Tim Smith <tsmith84@gmail.com>
2021-09-20 13:33:11 -0700

    Remove the data in the copyright
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/omnibus-ctl.rb
+++ lib/omnibus-ctl.rb
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012-2015 Chef Software, Inc.
+# Copyright:: Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
